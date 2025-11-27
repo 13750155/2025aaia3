8469a448fd6073f504589faa67e159dd2a88451f
13750155 <13750155@me.mcu.edu.tw>
2025-11-27 16:52:36 +0800

    week05

---
+++ week05/week05-3a.cpp
@@ -0,0 +1,13 @@
+/// week05-3a.cpp
+///CPE 第2題 UVA 483 倒過來
+#include <iostream>
+using namespace std;
+int main()
+{
+    string line; /// 一行字的字串
+
+    while ( getline(cin, line) ) { ///讀近來
+
+        cout << line << endl; ///part 2: output
+    }
+}
